6c8d1e0c53127219f51062dca83c2fc56717dca0
최혁재 <gurwo1007@gmail.com>
2021-01-19 15:49:06 +0900

    PostsRepository 추가 및 테스트 코드 추가

---
+++ src/main/java/com/book_reading_history/Application.java
@@ -0,0 +1,11 @@
+package com.book_reading_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
