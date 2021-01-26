e09f201cd89f5964789cca1ad1fb935b3c8aba41
최혁재 <gurwo1007@gmail.com>
2021-01-26 19:25:24 +0900

    빌드 시 모든 Entity에 createdDate, modifiedDate 자동 추가되도록 추가

--- src/main/java/com/book_reading_history/Application.java
+++ src/main/java/com/book_reading_history/Application.java
@@ -2,7 +2,9 @@ package com.book_reading_history;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
